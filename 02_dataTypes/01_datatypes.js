3aafe38c1a73d7c5d0517b3be32c4d428711c2ec
VAIBHAV <156915291+dot00Vaibhav@users.noreply.github.com>
2024-04-15 18:45:16 +0000

    lecture 05 datatypes

---
+++ 02_dataTypes/01_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//alert( 3 + 3 ) //we are using nodejs , not browser
+
+let name = "vaibhav"
+let age = 18
+let isLoggedIn = false
+
+
+//primitive data types
+
+// number => 2 to power 53
+//bigint :- for the big integers
+//string => ""
+// Boolean => true/false
+// null => standalone value :- representation of empty value
+// undefined => value not assigned
+// symbol : used to find the uniqueness
+
+//objects
+
+console.log(typeof name);//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
